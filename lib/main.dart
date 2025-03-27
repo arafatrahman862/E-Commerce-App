af9ab3016beef91874ea8d3a92e6964a4bceb066
habiba420 <sultana15-5528@diu.edu.bd>
2025-03-28 01:05:09 +0600

    habiba

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -108,6 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
+            Text('habiba'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
